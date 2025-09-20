534decda98cec7f275f5424e962f352b97c8913e
SOO <ssj060707+github@gmail.com>
2025-09-20 22:30:17 +0800

    yes fix 3

--- db/migrate/20250913152242_move_comments_to_polymorphic_location.rb
+++ db/migrate/20250913152242_move_comments_to_polymorphic_location.rb
@@ -5,6 +5,22 @@ class MoveCommentsToPolymorphicLocation < ActiveRecord::Migration[8.0]
 
   def up
     ActiveRecord::Base.transaction do
+      # there are remaining topics that start with version 0 before we changed it
+      # the mismatch is making the comments not go through
+      borked_topics = []
+
+      TopicInstance.find_each do |topic_instance|
+        if topic_instance.version == 0
+          borked_topics << topic_instance.topic
+        end
+      end
+
+      borked_topics.each do |topic|
+        topic.topic_instances.each do |topic_instance|
+          topic_instance.version += 1
+        end
+      end
+
       Comment.find_each do |comment|
         project_instance = ProjectInstance.find_by(project_id: comment.project_id, version: comment.project_version_number)
         topic_instance = TopicInstance.find_by(project_id: comment.project_id, version: comment.project_version_number)
